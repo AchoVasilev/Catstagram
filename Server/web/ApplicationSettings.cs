819e6d899e5e246a8f6e1bb849d8ed73e7df8cd3
Angel Vasilev <angavasilev@gmail.com>
2022-08-01 07:47:33 +0300

    project cleanup

---
+++ Server/web/ApplicationSettings.cs
@@ -0,0 +1,6 @@
+namespace web;
+
+public class ApplicationSettings
+{
+    public string Secret { get; set; }
+}
--- Server/web/Program.cs
+++ Server/web/Program.cs
@@ -1,18 +1,24 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using web;
 using web.Data;
+using web.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+
+var appSettings = builder.Configuration.GetSection("ApplicationSettings");
+builder.Services.Configure<ApplicationSettings>(appSettings);
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -21,24 +27,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
